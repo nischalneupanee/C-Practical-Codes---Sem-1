2ed7cb1a5c0a3e957d547648ef22026c65aef14c
Nischal <143786352+nischalneupanee@users.noreply.github.com>
2024-02-14 22:41:29 +0545

    Question 33

--- Pr_33_amount.c
+++ Pr_33_amount.c
@@ -15,5 +15,71 @@ customer.*/
 
 void main()
 {
-    
+    int mill, hand;
+    float ratem, rateh, amount = 0;
+    char choice = 'a',sel;
+    while (choice == 'a')
+    {
+        printf("Enter m - Mill Cloth ,h - handloom : ");
+        scanf(" %c",&sel);
+        switch (sel)
+        {
+        case 'm':
+            printf("Enter the amount of cloth : ");
+            scanf("%d",&mill);
+            if (mill <= 100)
+            {
+                ratem = 0.0;
+            }
+
+            else if (mill<=200)
+            {
+                ratem = 0.05;
+            }
+
+            else if (mill<=300)
+            {
+                ratem = 0.075;
+            }
+            else
+            {
+                ratem = 0.1;
+            }
+            
+            break;
+
+        case 'h':
+            printf("Enter the amount of handloom : ");
+            scanf("%d",&hand);
+            if (hand <= 100)
+            {
+                rateh = 0.05;
+            }
+
+            else if (hand <=200)
+            {
+                rateh = 0.075;
+            }
+
+            else if (hand <=300)
+            {
+                rateh = 0.1;
+            }
+            else
+            {
+                rateh = 0.15;
+            }
+            
+            break;
+        
+        default:
+        printf("Invalid Input");
+            break;
+        }
+        amount += (mill - (mill*ratem))+(hand - (hand*rateh)); 
+        printf("Enter a to add other or else to exit: ");
+        scanf(" %c", &choice);
+    }
+    printf("%.2f",amount);
+
 }
